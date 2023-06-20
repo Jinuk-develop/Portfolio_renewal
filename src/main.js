2d319931bc50c54c919dc00f05579c3a15423e99
Jinuk-develop <127473357+Jinuk-develop@users.noreply.github.com>
2023-06-20 21:15:10 +0900

    JS- HOME
    Home section opacity

--- src/main.js
+++ src/main.js
@@ -3,11 +3,19 @@
 
 // 스크롤시 Header에 다크 스크롤링 적용.
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+// const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', (event) => {
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
   }
+});
+
+//  아래로 스크롤시 Home section을 점차 투명하게
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
