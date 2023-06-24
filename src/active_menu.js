00123e299a5624a1df2bc9813a7338c21c31cd72
Jinuk-develop <127473357+Jinuk-develop@users.noreply.github.com>
2023-06-24 18:37:47 +0900

    JS - observer
    JS - observer 메뉴 아이템 활성화.

---
+++ src/active_menu.js
@@ -0,0 +1,70 @@
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#testimonial',
+  '#contact',
+];
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[href="${id}"]`)
+);
+const visibleSections = sectionIds.map(() => false);
+
+let activeNavItem = navItems[0];
+
+const options = {
+  rootMargin: '-20% 0px 0px 0px',
+  threshold: [0, 0.5, 0.98],
+};
+const observer = new IntersectionObserver(callback, options);
+sections.forEach((section) => observer.observe(section));
+
+function callback(entries) {
+  let selectLastOne; // flag 변수
+  entries.forEach((entry) => {
+    const index = sectionIds.indexOf(`#${entry.target.id}`);
+    visibleSections[index] = entry.isIntersecting;
+    selectLastOne =
+      index === sectionIds.length - 1 &&
+      entry.isIntersecting &&
+      entry.intersectionRatio >= 0.95;
+  });
+  const navIndex = selectLastOne
+    ? sectionIds.length - 1
+    : findFirstIntersecting(visibleSections);
+
+  selectNavItem(navIndex);
+}
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove('active');
+  activeNavItem = navItem;
+  activeNavItem.classList.add('active');
+}
+
+function findFirstIntersecting(intersections) {
+  const index = intersections.indexOf(true);
+  return index >= 0 ? index : 0;
+}
+// const options = {
+//   threshold: 0.9,
+// };
+// const observer = new IntersectionObserver(callback, options);
+// const sections = document.querySelectorAll('.section');
+// sections.forEach((section) => {
+//   observer.observe(section);
+// });
+
+// const headerMenu = document.querySelectorAll('.header__menu__item');
+// function callback(entries) {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       headerMenu.classList.add('active');
+//     } else {
+//       headerMenu.classList.remove('active');
+//     }
+//   });
+// }
